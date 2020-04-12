d039d3691cfb3679159121791b83188cf31170b8
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-12 17:20:57 +0300

    ccd3rdday1

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xCbtPeMxc7":
+        Script1();
+        break;
+      case "5XE0utMtw7v":
+        Script2();
+        break;
+      case "6YdHOqR3iXQ":
+        Script3();
+        break;
+      case "5oPCQs06YBf":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  window.print();
+}
+
